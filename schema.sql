c79bc39a3572a7ae6e8246fc45c844c56194040a
Nadine-ALmasri <nadinealmasri331994@gmail.com>
2023-05-07 18:25:02 +0300

    edited

--- schema.sql
+++ schema.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS newMovie;
 CREATE TABLE IF NOT EXISTS newMovie(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-   opinion VARCHAR(255)
+   release_date VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview VARCHAR(255)
   
 );
